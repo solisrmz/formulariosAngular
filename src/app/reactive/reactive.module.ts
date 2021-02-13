89743796cda3b81f75b3874dc93b4f2e6c4fc72e
Abraham <isolisrmz@gmail.com>
2021-02-13 14:04:29 -0600

    Se han maquetado los formularios para la seccion de manejo reactivo

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -5,12 +5,14 @@ import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [BasicosComponent, DinamicosComponent, SwitchesComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ]
 })
